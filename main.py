0ab14ed34cf07c3fa968b49b24280268385dacf7
student <student@E321-07>
2023-11-20 16:19:46 +0300

    new_commit

--- main.py
+++ main.py
@@ -1,36 +1,13 @@
-import sys
-from random import randint
-from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+def my_superfunction():
+    print('What an awesome print!')
 
 
-# noinspection PyArgumentList
-class MyWidget(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('Ui.ui', self)
-        self.pushButton.clicked.connect(self.drawEllipse)
-        self.f = False
-
-    def drawEllipse(self):
-        self.f = True
-        self.update()
-
-    def paintEvent(self, event):
-        if self.f:
-            q = QPainter()
-            q.begin(self)
-            x, y = randint(50, 100), randint(50, 100)
-            r = randint(20, 80)
-            q.setBrush(QColor(255, 204, 0))
-            q.drawEllipse(x, y, r, r)
-            q.end()
-
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    my_superfunction()
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    main()
